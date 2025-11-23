de38157db86d7bb2f3581d5310d505d593ddea8d
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-11-22 21:38:13 -0700

    Fix: Regenerate and Hide FireBase API Key

--- app/lib/firebase.js
+++ app/lib/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCg9dt7Dn90unlUH7tFLt3WIkcv9LIRVQI",
-  authDomain: "ratingguessr-v1-37586.firebaseapp.com",
-  projectId: "ratingguessr-v1-37586",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: "ratingguessr-v1-37586.firebasestorage.app",
   messagingSenderId: "588102126784",
-  appId: "1:588102126784:web:2c9fd7159376e63de6b125",
-  measurementId: "G-F1ECLBRWYF"
+  appId: "1:588102126784:web:7611d4d0f853d7a6e6b125",
+  measurementId: "G-9Z4TTTH5V4"
 };
 
 // Initialize Firebase
